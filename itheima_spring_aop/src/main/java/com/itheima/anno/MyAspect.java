0172247eb4abe730bfb52d2b5aef2a79a9a5b859
MyStudentGet <2920936986@qq.com>
2021-04-14 17:59:11 +0800

    First commit all.2021/4/11

--- itheima_spring_aop/src/main/java/com/itheima/anno/MyAspect.java
+++ itheima_spring_aop/src/main/java/com/itheima/anno/MyAspect.java
@@ -1,23 +1,28 @@
-package com.itheima.aop;
+package com.itheima.anno;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
 
+@Component("myAspect")
+@Aspect // 标注当前MyAspect是一个切面类
 public class MyAspect {
 
-
     // 前置增强
+    @Before("execution(* com.itheima.anno.*.*(..))")
     public void before(){
         System.out.println("前置增强。。。。。。");
     }
 
     // 后置增强
+//    @AfterReturning("execution(* com.itheima.anno.*.*(..))")
     public void afterReturning(){
         System.out.println("后置增强。。。。。。");
     }
 
     // 环绕增强
     // ProceedingJoinPoint：切点(正在执行的连接点)
+    @Around("execution(* com.itheima.anno.*.*(..))")
     public Object around(ProceedingJoinPoint pjp) throws Throwable {
         System.out.println("环绕前增强。。。。。。");
         //运行切点方法（切点方法可能有返回值）
@@ -27,11 +32,13 @@ public class MyAspect {
     }
 
     // 异常抛出增强
+    @AfterThrowing("execution(* com.itheima.anno.*.*(..))")
     public void afterThrowing(){
         System.out.println("异常抛出增强。。。。。。");
     }
 
     // 最终增强
+    @After("execution(* com.itheima.anno.*.*(..))")
     public void after(){
         System.out.println("最终增强。。。。。。");
     }
