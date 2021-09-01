57bdeea915a5698050a279b0031cd7e68aaa2d8b
MyStudentGet <2920936986@qq.com>
2021-09-01 14:41:04 +0800

    First commit all.2021/6/3

--- itheima_sping_anno/src/test/java/com/itheima/dao/impl/UserDaoImpl.java
+++ itheima_sping_anno/src/test/java/com/itheima/dao/impl/UserDaoImpl.java
@@ -2,9 +2,10 @@ package com.itheima.dao.impl;
 
 import com.itheima.dao.UserDao;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 //<bean id="userDao" class="com.itheima.dao.impl.UserDaoImpl"/>
-@Component("userDao")
+@Repository("userDao")
 public class UserDaoImpl implements UserDao {
 
     public void save() {
